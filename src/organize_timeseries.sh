1d94e15fd6b3adb5094ec7f8844a803298c7abe5
Manjari Narayan (SU) <mnarayan@users.noreply.github.com>
2018-01-28 13:08:58 -0800

    Fixed path specification bugs

--- src/organize_timeseries.sh
+++ src/organize_timeseries.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 DATADIR=data/cpac
+SRCDIR=src
 PIPELINE=pipeline_analysis
 ATLAS=CC200
 ATLASNAME=CC200
 
-# output only
-ls data/cpac/output/pipeline_analysis/*/roi_timeseries/*/*/*/*/*.csv | grep $ATLAS > path_roi_timeseries_mask_$ATLASNAME.txt
+# output with nuisance info directory
+# ls ${DATADIR}/output/pipeline_analysis/*/roi_timeseries/*/*/*/*/*.csv | grep $ATLAS > path_roi_timeseries_mask_$ATLASNAME.txt
 
-python3 collect_timeseries.py path_roi_timeseries_mask_$ATLASNAME.txt
+# output without nuisance info directory
+ls ${DATADIR}/output/pipeline_analysis/*/roi_timeseries/*/*/*/*.csv | grep $ATLAS > path_roi_timeseries_mask_$ATLASNAME.txt
+
+python3 ${SRCDIR}/collect_timeseries.py path_roi_timeseries_mask_$ATLASNAME.txt
 
 
